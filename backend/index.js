5dd43decb9f765dc551fa8bcadd8b2aa73797b56
ClaudiaBanciu <claudia.banciu20@gmail.com>
2023-12-20 23:38:18 +0200

    backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const groceryRoutes = require('./routes/grocery');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+app.use('/groceries', groceryRoutes);
+
+app.use(errorController.get404);
+
+app.use(errorController.get500);
+
+const ports = process.env.PORT || 3000;
+app.listen(ports, () => console.log(`listening on port ${ports}`));
